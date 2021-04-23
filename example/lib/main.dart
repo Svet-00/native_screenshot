dc2689571629f41528ab98947de04093aee177db
Svetozar Volkov <svetozar.volkov243@gmail.com>
2021-04-23 20:47:56 +0300

    Updated API to return image data instead of saving file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:native_screenshot/native_screenshot.dart';
@@ -6,69 +6,50 @@ import 'package:native_screenshot/native_screenshot.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-	@override
-	_MyAppState createState() => _MyAppState();
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-	Widget _imgHolder;
+  Uint8List? _imageData;
 
-	@override
-	void initState() {
-		super.initState();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('NativeScreenshot Example'),
+        ),
+        bottomNavigationBar: ButtonBar(
+          alignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Press to capture screenshot'),
+              onPressed: () async {
+                Uint8List? image = await NativeScreenshot.takeScreenshot();
+                setState(() => _imageData = image);
 
-		_imgHolder = Center(
-			child: Icon(Icons.image),
-		);
-	} // initState()
+                if (image == null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Error taking the screenshot :('),
+                      backgroundColor: Colors.red,
+                    ),
+                  ); // showSnackBar()
 
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			home: Scaffold(
-				appBar: AppBar(
-					title: Text('NativeScreenshot Example'),
-				),
-				bottomNavigationBar: ButtonBar(
-					alignment: MainAxisAlignment.center,
-					children: <Widget>[
-						ElevatedButton(
-							child: Text('Press to capture screenshot'),
-							onPressed: () async {
-								String path = await NativeScreenshot.takeScreenshot();
+                  return;
+                } // if error
 
-								debugPrint('Screenshot taken, path: $path');
-
-								if( path == null || path.isEmpty ) {
-									ScaffoldMessenger.of(context).showSnackBar(
-										SnackBar(
-											content: Text('Error taking the screenshot :('),
-											backgroundColor: Colors.red,
-										)
-									); // showSnackBar()
-
-									return;
-								} // if error
-
-								ScaffoldMessenger.of(context).showSnackBar(
-									SnackBar(
-										content: Text('The screenshot has been saved to: $path')
-									)
-								); // showSnackBar()
-
-								File imgFile = File(path);
-								_imgHolder = Image.file(imgFile);
-
-								setState(() {});
-							},
-						)
-					],
-				),
-				body: Container(
-					constraints: BoxConstraints.expand(),
-					child: _imgHolder,
-				),
-			),
-		);
-	} // build()
+                setState(() {});
+              },
+            )
+          ],
+        ),
+        body: Container(
+          constraints: BoxConstraints.expand(),
+          child: _imageData != null ? Center(child: Image.memory(_imageData!)) : Center(child: Icon(Icons.image)),
+        ),
+      ),
+    );
+  } // build()
 } // _MyAppState
--- lib/native_screenshot.dart
+++ lib/native_screenshot.dart
@@ -1,20 +1,20 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
 /// Class to capture screenshots with native code working on background
 class NativeScreenshot {
-	/// Comunication property to talk to the native background code.
-	static const MethodChannel _channel =
-			const MethodChannel('native_screenshot');
+  /// Comunication property to talk to the native background code.
+  static const MethodChannel _channel = const MethodChannel('native_screenshot');
 
-	/// Captures everything as is shown in user's device.
-	///
-	/// Returns [null] if an error ocurrs.
-	/// Returns a [String] with the path of the screenshot.
-	static Future<String?> takeScreenshot() async {
-		final String? path = await _channel.invokeMethod('takeScreenshot');
+  /// Captures everything as is shown in user's device.
+  ///
+  /// Returns [null] if an error occurs.
+  /// Returns a [Uint8List] with the image data.
+  static Future<Uint8List?> takeScreenshot() async {
+    final Uint8List? image = await _channel.invokeMethod('takeScreenshot');
 
-		return path;
-	} // takeScreenshot()
+    return image;
+  } // takeScreenshot()
 } // NativeScreenshot
